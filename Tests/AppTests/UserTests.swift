a03f505e483a427ea0b0216bd981f2a96317bdbe
Michael Critz <github@michaelcritz.com>
2020-08-23 02:40:56 -0700

    Tests: admin login / logout (#17)

--- Tests/AppTests/UserTests.swift
+++ Tests/AppTests/UserTests.swift
@@ -13,7 +13,27 @@ final class UserTests: XCTestCase {
     static var allTests = [
         ("testBadUserLogin", testBadUserLogin),
         ("testUserLogin", testUserLogin),
+        ("testLoginView", testLoginView),
+        ("testLogout", testLogout),
     ]
+    
+    fileprivate let htmlMediaType = HTTPMediaType(type: "text", subType: "html")
+    fileprivate let loginPath = "login/?redirect=admin"
+    
+    func testLoginView() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+
+        try app.test(.GET, "/login/") { res in
+            guard let contentType = res.content.contentType else {
+                XCTFail("Content must have a contentType")
+                return
+            }
+            XCTAssertEqual(contentType, htmlMediaType)
+            XCTAssertEqual(res.status, .ok)
+        }
+    }
     
     fileprivate func login(_ req: inout XCTHTTPRequest, username: String, password: String) throws {
         guard let password = try? Bcrypt.hash(password) else {
@@ -43,9 +63,10 @@ final class UserTests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
         
-        try app.test(.POST, "login/?redirect=admin", beforeRequest: { req in
+        try app.test(.POST, loginPath, beforeRequest: { req in
             try login(&req, username: "feather@binarybirds.com", password: "FeatherCMS")
         }, afterResponse: { res in
+            XCTAssertTrue(res.headers.setCookie != nil)
             XCTAssertEqual(res.status, .ok)
         })
     }
@@ -55,10 +76,48 @@ final class UserTests: XCTestCase {
         defer { app.shutdown() }
         try configure(app)
         
-        try app.test(.POST, "login/?redirect=admin", beforeRequest: { req in
+        try app.test(.POST, loginPath, beforeRequest: { req in
             try login(&req, username: "feAtHeR@BinArYBirDS.Com", password: "FeatherCMS")
         }, afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
+        })
+    }
+    
+    func testLogout() throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+        
+        var sessionCookie = HTTPCookies.Value(stringLiteral: "")
+        
+        try app.test(.POST, loginPath,
+                     beforeRequest: { req in
+                        try login(&req, username: "feather@binarybirds.com", password: "FeatherCMS")
+                     },
+                     afterResponse: { res in
+                        guard let firstCookie = res.headers.setCookie?["vapor-session"] else {
+                            XCTFail("no Session cookie")
+                            return
+                        }
+                        sessionCookie = firstCookie
+                     }
+        )
+        
+        try app.test(.GET, "logout", beforeRequest: { req in
+            req.headers.setCookie?["vapor-session"] = sessionCookie
+        },
+        afterResponse: { res in
+            let retrievedCookie = res.headers.setCookie?["vapor-session"]
+            XCTAssertNil(retrievedCookie,
+                         "No session cookie should exist after logout")
+        })
+        
+        try app.test(.GET, "admin", beforeRequest: { req in
+            req.headers.setCookie?["vapor-session"] = sessionCookie
+        },
+        afterResponse: { res in
+            XCTAssertNotEqual(res.status, .ok,
+                              "Should not be able to access a secured path after logout")
         })
     }
 }
