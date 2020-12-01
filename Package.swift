deaa16ca2378e29c05e58267c085152e3eced47a
Tibor Bödecs <mail.tib@gmail.com>
2020-12-01 21:53:04 +0100

    Updated deps, file module, fixed Feather target & ignore

--- Package.swift
+++ Package.swift
@@ -10,13 +10,14 @@ let package = Package(
         .executable(name: "Feather", targets: ["Feather"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor", from: "4.34.0"),
+        .package(url: "https://github.com/vapor/vapor", from: "4.36.0"),
         .package(url: "https://github.com/binarybirds/feather-core", from: "1.0.0-beta"),
         .package(url: "https://github.com/binarybirds/leaf-foundation", from: "1.0.0-beta"),
         /// drivers
         .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
-        .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.0.0"),
+        .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.2.0-beta"),
         /// modules
+        .package(name: "file-module", url: "https://github.com/feather-modules/file", from: "1.0.0-beta"),
         .package(name: "redirect-module", url: "https://github.com/feather-modules/redirect", from: "1.0.0-beta"),
         .package(name: "static-module", url: "https://github.com/feather-modules/static", from: "1.0.0-beta"),
         .package(name: "blog-module", url: "https://github.com/feather-modules/blog", from: "1.0.0-beta"),
@@ -25,6 +26,8 @@ let package = Package(
         .package(name: "markdown-module", url: "https://github.com/feather-modules/markdown", from: "1.0.0-beta"),
         .package(name: "analytics-module", url: "https://github.com/feather-modules/analytics", from: "1.0.0-beta"),
         .package(name: "sponsor-module", url: "https://github.com/feather-modules/sponsor", from: "1.0.0-beta"),
+        
+        .package(url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.3"),
     ],
     targets: [
         .target(name: "Feather", dependencies: [
@@ -34,6 +37,7 @@ let package = Package(
             .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
             .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
             /// modules
+            .product(name: "FileModule", package: "file-module"),
             .product(name: "RedirectModule", package: "redirect-module"),
             .product(name: "StaticModule", package: "static-module"),
             .product(name: "BlogModule", package: "blog-module"),
@@ -42,6 +46,8 @@ let package = Package(
             .product(name: "MarkdownModule", package: "markdown-module"),
             .product(name: "AnalyticsModule", package: "analytics-module"),
             .product(name: "SponsorModule", package: "sponsor-module"),
+            
+            .product(name: "Kanna", package: "Kanna"),
             /// yeah we only need this here, because SPM in Xcode 12 still can't resolve stuff...
             .product(name: "Vapor", package: "vapor"),
         ], exclude: [
---
+++ Sources/Feather/configure.swift
@@ -0,0 +1,125 @@
+//
+//  configure.swift
+//  Feather
+//
+//  Created by Tibor Bodecs on 2019. 12. 17..
+//
+
+import FeatherCore
+import LeafFoundation
+/// drivers
+import FluentSQLiteDriver
+import LiquidLocalDriver
+/// modules
+import FileModule
+import RedirectModule
+import StaticModule
+import BlogModule
+import SiteModule
+import SwiftyModule
+import MarkdownModule
+import AnalyticsModule
+import SponsorModule
+
+
+public func configure(_ app: Application) throws {
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    /*
+    LeafFileMiddleware.defaultMediaType = .html
+    LeafFileMiddleware.processableExtensions = ["leaf", "html", "css", "js"]
+    LeafFileMiddleware[.css] = [
+        "background": "green",
+        "padding": "16px",
+    ]
+    LeafFileMiddleware.contexts = [
+        .css: [
+            "background": "#eee",
+            "padding": "16px",
+        ],
+        .html: [
+            "title": "Hello world!"
+        ],
+    ]
+
+    if let lfm = LeafFileMiddleware(publicDirectory: app.directory.publicDirectory) {
+        app.middleware.use(lfm)
+    }
+    */
+
+    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+
+    app.fileStorages.use(.local(publicUrl: Application.baseUrl,
+                                publicPath: app.directory.publicDirectory,
+                                workDirectory: "assets"), as: .local)
+
+    app.routes.defaultMaxBodySize = "10mb"
+
+    app.sessions.use(.fluent)
+    app.migrations.add(SessionRecord.migration)
+    app.middleware.use(SlashMiddleware())
+    app.middleware.use(app.sessions.middleware)
+    app.middleware.use(LeafFoundationMiddleware())
+    
+
+    let modules: [ViperModule] = [
+        ///core modules (do not remove them)
+        UserBuilder(),
+        SystemBuilder(),
+        MenuBuilder(),
+        AdminBuilder(),
+        ApiBuilder(),
+        FrontendBuilder(),
+        /// user modules (feel free to add / remove)
+        FileBuilder(),
+        RedirectBuilder(),
+        StaticBuilder(),
+        BlogBuilder(),
+        SiteBuilder(),
+        AnalyticsBuilder(),
+        SponsorBuilder(),
+        SwiftyBuilder(),
+        MarkdownBuilder(),
+    ].map { $0.build() }
+    
+    let defaultSource = NIOLeafFiles(fileio: app.fileio,
+                               limits: [.requireExtensions],
+                               sandboxDirectory: app.directory.resourcesDirectory,
+                               viewDirectory: app.directory.viewsDirectory,
+                               defaultExtension: "html")
+
+    let multipleSources = LeafSources()
+    try multipleSources.register(using: defaultSource)
+
+    for module in modules {
+        guard let url = module.bundleUrl else { continue }
+
+        let moduleSource = ViperBundledLeafSource(module: module.name,
+                                                  rootDirectory: url.path.withTrailingSlash,
+                                                  templatesDirectory: "Templates",
+                                                  fileExtension: "html",
+                                                  fileio: app.fileio)
+
+        try multipleSources.register(source: "\(module.name)-module-bundle", using: moduleSource)
+    }
+
+    LeafEngine.sources = multipleSources
+    LeafEngine.useLeafFoundation()
+    LeafEngine.entities.use(ResolveLeafEntity(), asMethod: "resolve")
+    LeafEngine.entities.use(InvokeHookLeafEntity(), asFunction: "InvokeHook")
+    LeafEngine.entities.use(InvokeAllHooksLeafEntity(), asFunction: "InvokeAllHooks")
+    LeafEngine.entities.use(InlineSvg(iconset: "feather-icons"), asFunction: "svg")
+    LeafRenderer.Option.timeout = 1.000 // 1000ms
+
+    if app.isDebug {
+        LeafRenderer.Option.caching = .bypass
+    }
+    app.views.use(.leaf)
+    
+    try app.viper.use(modules)
+
+    app.middleware.use(FeatherCoreLeafExtensionMiddleware())
+    app.middleware.use(ViperLeafScopesMiddleware())    
+    
+    try app.autoMigrate().wait()
+}
