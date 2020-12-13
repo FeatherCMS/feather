501d13bdba675da9611d2566d97d153ce8afb3e9
MARTIN Denis <denis@irlmobile.com>
2020-12-13 10:27:23 +0100

    Moved DB logic out of the main. It looks cleaner.

--- Sources/Feather/main.swift
+++ Sources/Feather/main.swift
@@ -6,10 +6,6 @@
 //
 
 import FeatherCore
-import FluentSQLiteDriver
-import FluentPostgresDriver
-import FluentMySQLDriver
-import LiquidLocalDriver
 
 import FileModule
 import RedirectModule
@@ -23,45 +19,14 @@ import MarkdownModule
 let feather = try Feather()
 defer { feather.stop() }
 
-let dbconfig: DatabaseConfigurationFactory
-let dbID: DatabaseID
-switch Environment.get("DBTYPE") {
-case "mysql":
-        dbconfig = .mysql(hostname: Environment.fetch("SQL_HOST"),
-                                             port: Int(Environment.get("SQL_PORT") ?? "3306")!,
-                                             username: Environment.fetch("SQL_USER"),
-                                             password: Environment.fetch("SQL_PASSWORD"),
-                                             database: Environment.fetch("SQL_DATABASE"),
-                                             tlsConfiguration: .forClient(certificateVerification: .none))
-        dbID = .mysql
-    break
-case "postgres":
-        dbconfig = .postgres(hostname: Environment.fetch("SQL_HOST"),
-                             port: Int(Environment.get("SQL_PORT") ?? "5432")!,
-                             username: Environment.fetch("SQL_USER"),
-                             password: Environment.fetch("SQL_PASSWORD"),
-                             database: Environment.fetch("SQL_DATABASE"))
-        dbID = .psql
-    break
-default:
-    dbconfig = .sqlite(.file("db.sqlite"))
-    dbID     = .sqlite
-    break
-}
-
-try feather.configure(database: dbconfig,
-                      databaseId: dbID,
-                      fileStorage: .local(publicUrl: Application.baseUrl, publicPath: Application.Paths.public, workDirectory: "assets"),
-                      fileStorageId: .local,
-                      maxUploadSize: ByteCount(stringLiteral: Environment.get("MAX_BODYSIZE") ?? "10mb"),
-                      modules: [
-                        FileBuilder(),
-                        RedirectBuilder(),
-                        BlogBuilder(),
-                        AnalyticsBuilder(),
-//                        AggregatorBuilder(),
-                        SponsorBuilder(),
-                        SwiftyBuilder(),
-                        MarkdownBuilder(),
+try feather.configureWithEnv(modules: [
+                                FileBuilder(),
+                                RedirectBuilder(),
+                                BlogBuilder(),
+                                AnalyticsBuilder(),
+//                                AggregatorBuilder(),
+                                SponsorBuilder(),
+                                SwiftyBuilder(),
+                                MarkdownBuilder(),
                       ])
 try feather.start()
