d77a34921a97fa7fcc7150787399646d51487f2b
Tibor Bodecs <mail.tib@gmail.com>
2022-03-05 14:21:25 +0100

    dep updates

--- Package.swift
+++ Package.swift
@@ -10,17 +10,16 @@ let package = Package(
         
     ],
     dependencies: [
-        .package(path: "../feather-core"),
-        .package(path: "../user-module"),
-        .package(path: "../web-module"),
+        .package(url: "https://github.com/feathercms/feather-core", .branch("dev")),
+        .package(url: "https://github.com/feathercms/user-module", .branch("main")),
+        .package(url: "https://github.com/feathercms/web-module", .branch("main")),
+
         .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
         .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.3.0"),
         .package(url: "https://github.com/binarybirds/mail-aws-driver", from: "0.0.1"),
     ],
     targets: [
-        
         .executableTarget(name: "App", dependencies: [
-
             .product(name: "Feather", package: "feather-core"),
             .product(name: "UserModule", package: "user-module"),
             .product(name: "WebModule", package: "web-module"),
