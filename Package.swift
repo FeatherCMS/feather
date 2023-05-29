7134ec86e117f65f067f59f27b7d5b32e41b0878
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-05-29 08:19:25 -0500

    refactor dep urls for merge

--- Package.swift
+++ Package.swift
@@ -10,34 +10,10 @@ let package = Package(
         .executable(name: "App", targets: ["App"])
     ],
     dependencies: [
-//        .package(path: "../feather-core"),
-//        .package(path: "../user-module"),
-//        .package(path: "../web-module"),
-//        .package(path: "../redirect-module"),
-		
-		.package(url: "https://github.com/Rando-Coderissian/feather-core", .branch("test-refactored-modules")),
-		.package(url: "https://github.com/Rando-Coderissian/user-module", .branch("test-refactor-modules")),
-		.package(url: "https://github.com/Rando-Coderissian/web-module", .branch("test-refactor-modules")),
-		.package(url: "https://github.com/Rando-Coderissian/redirect-module", .branch("test-refactor-modules")),
-		
-		
-		///        .package(url: "https://github.com/feathercms/feather-core", .branch("dev")),
-		///        .package(url: "https://github.com/feathercms/user-module", .branch("dev")),
-		///        .package(url: "https://github.com/feathercms/web-module", .branch("dev")),
-		///        .package(url: "https://github.com/feathercms/redirect-module", .branch("dev")),
-
-//		.package(path: "../analytics-module"),
-////		.package(path: "../aggregator-module"),
-//		.package(path: "../blog-module"),
-//		.package(path: "../markdown-module"),
-//		.package(path: "../swifty-module"),
-		
-		.package(url: "https://github.com/Rando-Coderissian/analytics-module", .branch("test-refactor-modules")),
-		.package(url: "https://github.com/Rando-Coderissian/blog-module", .branch("test-refactor-modules")),
-		.package(url: "https://github.com/Rando-Coderissian/markdown-module", .branch("test-refactor-modules")),
-		.package(url: "https://github.com/Rando-Coderissian/swifty-module", .branch("test-refactor-modules")),
-//		.package(url: "https://github.com/Rando-Coderissian/aggregator-module", .branch("test-refactor-modules")),
-		
+		.package(url: "https://github.com/feathercms/feather-core", .branch("dev")),
+		 .package(url: "https://github.com/feathercms/user-module", .branch("dev")),
+		 .package(url: "https://github.com/feathercms/web-module", .branch("dev")),
+		 .package(url: "https://github.com/feathercms/redirect-module", .branch("dev")),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
         .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.3.0"),
         .package(url: "https://github.com/binarybirds/mail-aws-driver", from: "0.0.1"),
@@ -48,13 +24,6 @@ let package = Package(
             .product(name: "UserModule", package: "user-module"),
             .product(name: "WebModule", package: "web-module"),
             .product(name: "RedirectModule", package: "redirect-module"),
-            
-			.product(name: "AnalyticsModule", package: "analytics-module"),
-//			.product(name: "AggregatorModule", package: "aggregator-module"),
-			.product(name: "BlogModule", package: "blog-module"),
-			.product(name: "SwiftyModule", package: "swifty-module"),
-			.product(name: "MarkdownModule", package: "markdown-module"),
-			
             .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
             .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
             .product(name: "MailAwsDriver", package: "mail-aws-driver"),
